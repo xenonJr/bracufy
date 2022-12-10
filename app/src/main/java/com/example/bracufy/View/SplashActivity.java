936ee66ec5e9e3213ecb8e77e421f5ba4ca55a14
zkxenon <zkxenon98@gmail.com>
2022-12-10 23:15:38 +0600

    init

---
+++ app/src/main/java/com/example/bracufy/View/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.bracufy.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bracufy.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+    }
+}
