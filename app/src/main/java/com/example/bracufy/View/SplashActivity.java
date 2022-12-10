df1f474aa5eb0e0829d8e5784b885a02921c3cb5
zkxenon <zkxenon98@gmail.com>
2022-12-11 00:21:09 +0600

    added phone login

--- app/src/main/java/com/example/bracufy/View/SplashActivity.java
+++ app/src/main/java/com/example/bracufy/View/SplashActivity.java
@@ -2,16 +2,31 @@ package com.example.bracufy.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.bracufy.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+
+    ImageButton phoneSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
+
+        phoneSignIn = findViewById(R.id.splash_phone);
+
+        phoneSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this,SignInPhone.class));
+            }
+        });
 
 
     }
